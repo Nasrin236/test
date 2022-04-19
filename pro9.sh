a1d7b64e6895153bd64147d3dcbf9a52a0425bdf
abhi <abhijithyams123@gmail.com>
2022-04-19 20:16:18 +0530

    9

---
+++ pro9.sh
@@ -0,0 +1,30 @@
+echo " enter the digits "
+read c
+x=$c
+sum=0
+r=0
+n=0
+
+while [ $x -gt 0 ]
+do
+r=`expr $x % 10`
+n=`expr $r \* $r \* $r `
+sum=`expr $sum + $n`
+x=`expr $x / 10`
+done
+if [ $sum -eq $c ] 
+then 
+echo " armstrong"
+else 
+echo " not armstrong"
+fi
+
+
+
+
+
+
+
+
+
+
